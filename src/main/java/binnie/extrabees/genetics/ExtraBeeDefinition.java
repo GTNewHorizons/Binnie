8b07f1ff4f2a3e5ed6aaeca1f7027d0e6789c3ea
Runakai1 <Runatson@gmail.com>
2022-12-05 17:09:42 +0100

    Blaickmailed to remove the blackmailed grouping

--- src/main/java/binnie/extrabees/genetics/ExtraBeeDefinition.java
+++ src/main/java/binnie/extrabees/genetics/ExtraBeeDefinition.java
@@ -1686,13 +1686,13 @@ public enum ExtraBeeDefinition implements IBeeDefinition {
                                     "BlueWeabo",
                                     "Lewis_Saber",
                                     "True_Aurastorm",
+                                    "mitchej123",
                                     "minecraft7771",
                                     "_Timbo",
                                     "kuba6000",
                                     "Alrightsc",
                                     "YeetYeetDatBoi",
                                     "DreamMasterXXL",
-                                    "mitchej123",
                                     "Colen"));
         }
     },
