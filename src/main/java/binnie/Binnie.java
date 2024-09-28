533b41cae87e5426f7c4976a8534eccad70b984c
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2024-09-28 18:55:13 -0400

    Remove unused item classes

--- src/main/java/binnie/Binnie.java
+++ src/main/java/binnie/Binnie.java
@@ -1,14 +1,12 @@
 package binnie;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import binnie.core.ManagerBase;
 import binnie.core.genetics.ManagerGenetics;
-import binnie.core.item.ManagerItem;
 import binnie.core.liquid.ManagerLiquid;
 import binnie.core.machines.ManagerMachine;
 import binnie.core.resource.ManagerResource;
+import java.util.ArrayList;
+import java.util.List;
 
 public final class Binnie {
 
@@ -16,6 +14,5 @@ public final class Binnie {
     public static ManagerGenetics Genetics = new ManagerGenetics();
     public static ManagerLiquid Liquid = new ManagerLiquid();
     public static ManagerMachine Machine = new ManagerMachine();
-    public static ManagerItem Item = new ManagerItem();
     public static ManagerResource Resource = new ManagerResource();
 }
