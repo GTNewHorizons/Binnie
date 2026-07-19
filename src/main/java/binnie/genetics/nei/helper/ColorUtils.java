4760600e94a8f15e51e025acc049d6b97366b570
Ranzu <66495944+Ranzuu@users.noreply.github.com>
2026-07-19 13:48:41 +0200

    Implement ColorUtils

---
+++ src/main/java/binnie/genetics/nei/helper/ColorUtils.java
@@ -0,0 +1,13 @@
+package binnie.genetics.nei.helper;
+
+import com.gtnewhorizon.gtnhlib.color.ColorResource;
+
+public class ColorUtils {
+
+    private static final ColorResource.Factory color = new ColorResource.Factory("binnie");
+
+    public static final ColorResource
+    // spotless:off
+        NeiDisplayText  = color.rgb("NeiDisplayText",    "0xFFFFFF");
+    // spotless:on
+}
